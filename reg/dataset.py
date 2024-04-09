882061e89050df4574e38639babc87aa9533f6aa
Jamie Temple <jamie-temple@live.de>
2024-04-09 10:55:57 +0200

    feat(dataset): make series_len configurable

--- reg/dataset.py
+++ reg/dataset.py
@@ -1,8 +1,7 @@
-import os, glob, torch
+import glob, torch
 import numpy as np
 import scipy.io as spio
 import pytorch_lightning as pl
-import torchio as tio
 
 from torch.utils.data import Dataset, DataLoader, random_split
 
@@ -24,7 +23,11 @@ def reader(path, max=192):
     return data
 
 class LungDataset(Dataset):
-    def __init__(self, train=True, val=False, split=(0.8, 0.1), seed=42):
+    def __init__(self, train=True, val=False, split=(0.8, 0.1), seed=42, series_len=192):
+        assert series_len % 8 == 0, "series_len must be divisible by 8"
+        assert not (train and val), "Either train or val must be True, not both"
+
+        self.series_len = series_len
         subjects = glob.glob("/media/agjvc_rad3/_TESTKOLLEKTIV/Daten/Daten/*/")
         
         total_len = len(subjects)
@@ -47,17 +50,20 @@ class LungDataset(Dataset):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
-        data = reader(self.image_paths[idx])
+        data = reader(self.image_paths[idx], self.series_len)
         ndat = normalize(data).astype(np.float32)
         return torch.from_numpy(ndat)
 
 class LungDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size=1, num_workers=1, pin_memory=True):
+    def __init__(self, batch_size=1, num_workers=1, pin_memory=True, seed=42, split=(0.8, 0.1), series_len=192):
         super().__init__()
 
         self.batch_size = batch_size
         self.pin_memory = pin_memory
         self.num_workers = num_workers
+        self.seed = seed
+        self.split = split
+        self.series_len = series_len
 
         self.train_data = None
         self.val_data = None
@@ -68,9 +74,9 @@ class LungDataModule(pl.LightningDataModule):
         pass
 
     def setup(self, stage=None):
-        self.train_set = LungDataset(train=True, val=False)
-        self.val_set = LungDataset(train=False, val=True)
-        self.test_set = LungDataset(train=False, val=False)
+        self.train_set = LungDataset(train=True, val=False, split=self.split, seed=self.seed, series_len=self.series_len)
+        self.val_set = LungDataset(train=False, val=True, split=self.split, seed=self.seed, series_len=self.series_len)
+        self.test_set = LungDataset(train=False, val=False, split=self.split, seed=self.seed, series_len=self.series_len)
 
     def train_dataloader(self):
         return DataLoader(
