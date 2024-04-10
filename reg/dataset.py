8f309d057a83ace8993ad4b58a7b226ee00a6a64
Jamie Temple <jamie-temple@live.de>
2024-04-10 14:55:27 +0200

    feat(project): make data mod configurable

--- reg/dataset.py
+++ reg/dataset.py
@@ -5,11 +5,13 @@ import pytorch_lightning as pl
 
 from torch.utils.data import Dataset, DataLoader, random_split
 
+
 def normalize(image):
     image_min = image.min()
     image_max = image.max()
-    normalized = (image - image_min) / (image_max - image_min)  
+    normalized = (image - image_min) / (image_max - image_min)
     return normalized
+
 
 def standardize(image):
     image_mean = image.mean()
@@ -17,19 +19,24 @@ def standardize(image):
     normalized = (image - image_mean) / image_std
     return normalized
 
-def reader(path, max=192):
-    dcm = spio.loadmat(path)["dcm"][:max]
-    data = np.transpose(dcm, (1, 2, 0))[None, :, :, :]# C, W, H, t
+
+def reader(path, end=192):
+    dcm = spio.loadmat(path)["dcm"][:end]
+    data = np.transpose(dcm, (1, 2, 0))[None, :, :, :]  # C, W, H, t
     return data
 
+
 class LungDataset(Dataset):
-    def __init__(self, train=True, val=False, split=(0.8, 0.1), seed=42, series_len=192):
+    def __init__(self, train=True, val=False, split=(0.8, 0.1), seed=42, series_len=192, mod="norm"):
         assert series_len % 8 == 0, "series_len must be divisible by 8"
         assert not (train and val), "Either train or val must be True, not both"
+        assert mod == "norm" or mod == "std" or mod is None, "mod can be 'norm', 'std' or None"
+
+        self.mod = mod
 
         self.series_len = series_len
         subjects = glob.glob("/media/agjvc_rad3/_TESTKOLLEKTIV/Daten/Daten/*/")
-        
+
         total_len = len(subjects)
         train_len = int(total_len * split[0])
         val_len = int(total_len * split[1])
@@ -51,11 +58,20 @@ class LungDataset(Dataset):
 
     def __getitem__(self, idx):
         data = reader(self.image_paths[idx], self.series_len)
-        ndat = normalize(data).astype(np.float32)
-        return torch.from_numpy(ndat)
+
+        if self.mod == "norm":
+            ndat = normalize(data)
+        elif self.mod == "std":
+            ndat = standardize(data)
+        else:
+            ndat = data
+
+        return torch.from_numpy(ndat.astype(np.float32))
+
 
 class LungDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size=1, num_workers=1, pin_memory=True, seed=42, split=(0.8, 0.1), series_len=192):
+    def __init__(self, batch_size=1, num_workers=1, pin_memory=True, seed=42, split=(0.8, 0.1), series_len=192,
+                 mod="norm"):
         super().__init__()
 
         self.batch_size = batch_size
@@ -64,19 +80,40 @@ class LungDataModule(pl.LightningDataModule):
         self.seed = seed
         self.split = split
         self.series_len = series_len
+        self.mod = mod
 
-        self.train_data = None
-        self.val_data = None
-        self.test_data = None
+        self.train_set = None
+        self.val_set = None
+        self.test_set = None
 
     def prepare_date(self):
         # not needed as the data is not downloaded
         pass
 
     def setup(self, stage=None):
-        self.train_set = LungDataset(train=True, val=False, split=self.split, seed=self.seed, series_len=self.series_len)
-        self.val_set = LungDataset(train=False, val=True, split=self.split, seed=self.seed, series_len=self.series_len)
-        self.test_set = LungDataset(train=False, val=False, split=self.split, seed=self.seed, series_len=self.series_len)
+        self.train_set = LungDataset(
+            train=True,
+            val=False,
+            split=self.split,
+            seed=self.seed,
+            series_len=self.series_len,
+            mod=self.mod)
+
+        self.val_set = LungDataset(
+            train=False,
+            val=True,
+            split=self.split,
+            seed=self.seed,
+            series_len=self.series_len,
+            mod=self.mod)
+
+        self.test_set = LungDataset(
+            train=False,
+            val=False,
+            split=self.split,
+            seed=self.seed,
+            series_len=self.series_len,
+            mod=self.mod)
 
     def train_dataloader(self):
         return DataLoader(
