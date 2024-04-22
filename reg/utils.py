f08fdb565c890c0fddb547ff6425b6743041c9ec
Jamie Temple <jamie-temple@live.de>
2024-04-22 10:07:27 +0200

    fix(project): properly parse input

--- reg/utils.py
+++ reg/utils.py
@@ -1,8 +1,8 @@
 from models.transmorph import TransMorph
+from configs import CONFIG_TM
+from metrics import CONFIGS_IMAGE_LOSS, CONFIGS_FLOW_LOSS
 from models.transmorph_bayes import TransMorphBayes
 from models.transmorph_bspline import TransMorphBspline
-from metrics import CONFIGS_IMAGE_LOSS, CONFIGS_FLOW_LOSS
-from configs import CONFIG_TM
 import torch
 
 CONFIGS_OPTIMIZER = {
@@ -10,9 +10,9 @@ CONFIGS_OPTIMIZER = {
 }
 
 
-def load_losses(image_losses, flow_losses):
-    image_losses = [str.split(s, ":") for s in str.split(str.lower(image_losses), "&")]
-    flow_losses = [str.split(s, ":") for s in str.split(str.lower(flow_losses), "&")]
+def load_losses(image_losses, flow_losses, delimiter=":"):
+    image_losses = [str.split(s, delimiter) for s in str.split(str.lower(image_losses), "&")]
+    flow_losses = [str.split(s, delimiter) for s in str.split(str.lower(flow_losses), "&")]
     return image_losses, flow_losses
 
 
@@ -20,7 +20,7 @@ def load_model_params(model_name, image_losses, flow_losses, optimizer_name, ser
     criteria_image = [(CONFIGS_IMAGE_LOSS[loss], float(weight)) for loss, weight in image_losses]
     criteria_flow = [(CONFIGS_FLOW_LOSS[loss], float(weight)) for loss, weight in flow_losses]
     criterion_disp = None
-    optimizer = CONFIGS_OPTIMIZER[optimizer_name]
+    optimizer = CONFIGS_OPTIMIZER[optimizer_name] if optimizer_name is not None else None
 
     config = CONFIG_TM[model_name]
     config.img_size = (*config.img_size[:-1], series_len)
