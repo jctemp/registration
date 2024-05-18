019691596fe58c1dc403a024a8cea7412019a237
Jamie Temple <jamie-temple@live.de>
2024-05-18 10:27:21 +0200

    fix(configs): add tm identity model

--- reg/transmorph/configs/__init__.py
+++ reg/transmorph/configs/__init__.py
@@ -1,7 +1,9 @@
 from .transmorph import CONFIGS as CONFIG_DEFAULT
 from .transmorph_bayes import CONFIGS as CONFIG_BAYES
 from .transmorph_bspline import CONFIGS as CONFIG_BSPLINE
+from .transmorph_identity import CONFIGS as CONFIG_IDENTITY
 
 CONFIG_TM = {}
 CONFIG_TM.update(CONFIG_DEFAULT)
 CONFIG_TM.update(CONFIG_BAYES)
+CONFIG_TM.update(CONFIG_IDENTITY)
